9521707e2d7fa282f2229adb25b8201d0be17761
Chloe <138293556+chloe-pfr@users.noreply.github.com>
2023-07-07 13:08:14 -0400

    .

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,20 @@ import { Context, Hono } from "hono";
 import { cors } from "hono/cors";
 import { bearerAuth } from "hono/bearer-auth";
 import { StatusCode } from "hono/utils/http-status";
-import { plausible } from "./analytics";
 
 type Bindings = {
   KV: KVNamespace;
-  TOKEN: string;
-  PLAUSIBLE_DOMAIN: string | undefined;
+  AUTH_TOKEN: string;
+
+  REDIREC_GEN200: string | undefined;
+  MESSAGE_GEN200: string | undefined;
+  CONSOLE_GEN200: string | undefined;
+  CONSOLE_SUC301: string | undefined;
+  MESSAGE_ERR400: string | undefined;
+  CONSOLE_ERR400: string | undefined;
+  REDIREC_ERR404: string | undefined;
+  MESSAGE_ERR404: string | undefined;
+  CONSOLE_ERR404: string | undefined;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -17,25 +25,29 @@ async function err(code: number, c: Context): Promise<Response> {
   return c.newResponse(await cat.arrayBuffer(), code as StatusCode);
 }
 
-app.get("/", (c) => c.text("hello hono, this is rinku! ▲"));
+app.get("/", (c) => {
+  if (c.env.REDIREC_GEN200 == undefined) {
+    c.text({ c.env.MESSAGE_GEN200 });
+  } else {
+
+  }
+  console.log(c.env.CONSOLE_GEN200);
+  
+
+});
 
 app.use("/api/*", cors());
 app.use("/api/*", async (c, next) => {
-  const auth = bearerAuth({ token: c.env.TOKEN })
+  const auth = bearerAuth({ token: c.env.AUTH_TOKEN })
   return auth(c, next)
 });
 
 app.delete("/*", async (c, next) => {
-  const auth = bearerAuth({ token: c.env.TOKEN })
+  const auth = bearerAuth({ token: c.env.AUTH_TOKEN })
   return auth(c, next)
 });
 
-app.all("/favicon.ico", async (c) => {
-  let cat = await fetch("https://s3.natalie.sh/nano/favicon.ico");
-  return c.newResponse(await cat.arrayBuffer());
-})
-
-app.get("/all", async (c) => {
+app.get("/api/all", async (c) => {
   let kv = await c.env.KV.list()
   return c.json(kv)
 })
@@ -94,27 +106,9 @@ app.patch("/:key", async (c) => {
   return c.text("Modified " + c.req.param("key"));
 });
 
-app.get("/js/script.js", async (c) => {
-  // assuming plausible, if not needed return a *descriptive* error
-  if(c.env.PLAUSIBLE_DOMAIN == undefined) return err(412, c)
-  console.log(c.req.url.replace(new URL(c.req.url).host, c.env.PLAUSIBLE_DOMAIN))
-  let response = await fetch(c.req.url.replace(new URL(c.req.url).host, c.env.PLAUSIBLE_DOMAIN));
-  return response
-});
-
-app.post("/api/event", async (c) => {
-  // assuming for plausible
-  if(c.env.PLAUSIBLE_DOMAIN == undefined) return err(412, c)
-  const request = c.req
-  request.headers.delete('cookie');
-  return await fetch("https://plausible.io/api/event", request);
-})
-
 app.get("/:key", async (c) => {
   let r = await c.env.KV.get(c.req.param("key"));
   console.log(new URL(c.req.url).host)
-
-  if (c.env.PLAUSIBLE_DOMAIN != undefined) c.executionCtx.waitUntil(plausible(c));
 
   if (r == null) return await err(404, c);
 
